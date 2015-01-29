2b69c6edca96d49b984f49a17172479ff973aee5
Martin Thoresen <martinhath@gmail.com>
2015-01-29 16:29:38 +0100

    Done (?) with header parsing
    Compiles. Is not tested, and ignores headers without ':'.

--- http/src/http/server.rs
+++ http/src/http/server.rs
@@ -3,6 +3,10 @@
 use std::io::{TcpListener, TcpStream};
 use std::str;
 
+use std::collections::HashMap;
+
+use utils;
+
 pub fn print_stuff() {
     println!("module fuck yeah")
 }
@@ -16,22 +20,36 @@ pub fn new_conn(mut stream: TcpStream) {
     println!("{}", msg)
 }
 
-
-fn read_to_crlf(stream: &mut TcpStream) -> String{
+fn read_to_crlf(stream: &mut TcpStream) -> String {
     const BUFFER_SIZE: usize = 4096;
     let mut buffer: &mut [u8; BUFFER_SIZE] = &mut [0; BUFFER_SIZE];
     let n = stream.read(buffer).unwrap();
 
     if n >= 4096 {
         panic!("Buffer full!")
-    }else{
+    } else {
         println!("read {} bytes", n)
     }
 
     let s = String::from_str(str::from_utf8(buffer).unwrap());
     println!("------MESSAGE------");
-    println!(s);
-    println!("--------END--------");
+    println!("{}", s);
+    println!("--------END--------\n\n");
     s
 }
 
+fn parse_headers(headers: &str) -> HashMap<&str, &str> {
+   let mut keystore = HashMap::new(); 
+   for l in headers.lines() {
+        let i: Option<usize> = utils::contains(l, ':');
+        match i {
+            Some(n) => {
+                let k = &l[..n];
+                let v = &l[n+1..];
+                keystore.insert(k, v);
+            },
+            None(..) => {}
+        }
+   }
+   keystore
+}
---
+++ http/src/http/utils.rs
@@ -0,0 +1,12 @@
+pub fn contains(string: &str, chr: char) -> Option<usize> {
+    let mut i: usize = 0;
+    for c in string.chars() {
+        if c == chr {
+            return Some(i);
+        }else
+        {
+            i += 1;
+        }
+    }
+    None
+}
